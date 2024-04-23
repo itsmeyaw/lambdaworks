1b42bc666be11aa1416afaf0d08cbac014ff2eba
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-04-24 00:17:41 +0200

    change: remove the interpolate function

--- math/src/polynomial/mod.rs
+++ math/src/polynomial/mod.rs
@@ -46,53 +46,8 @@ impl<F: IsField> Polynomial<FieldElement<F>> {
     pub fn interpolate(
         xs: &[FieldElement<F>],
         ys: &[FieldElement<F>],
-    ) -> Result<Self, InterpolateError> {
-        // TODO: try to use the type system to avoid this assert
-        if xs.len() != ys.len() {
-            return Err(InterpolateError::UnequalLengths(xs.len(), ys.len()));
-        }
-        if xs.is_empty() {
-            return Ok(Polynomial::new(&[]));
-        }
-
-        let mut denominators = Vec::with_capacity(xs.len() * (xs.len() - 1) / 2);
-        let mut indexes = Vec::with_capacity(xs.len());
-
-        let mut idx = 0;
-
-        for (i, xi) in xs.iter().enumerate().skip(1) {
-            indexes.push(idx);
-            for xj in xs.iter().take(i) {
-                if xi == xj {
-                    return Err(InterpolateError::NonUniqueXs);
-                }
-                denominators.push(xi - xj);
-                idx += 1;
-            }
-        }
-
-        FieldElement::inplace_batch_inverse(&mut denominators).unwrap();
-
-        let mut result = Polynomial::zero();
-
-        for (i, y) in ys.iter().enumerate() {
-            let mut y_term = Polynomial::new(&[y.clone()]);
-            for (j, x) in xs.iter().enumerate() {
-                if i == j {
-                    continue;
-                }
-                let denominator = if i > j {
-                    denominators[indexes[i - 1] + j].clone()
-                } else {
-                    -&denominators[indexes[j - 1] + i]
-                };
-                let denominator_poly = Polynomial::new(&[denominator]);
-                let numerator = Polynomial::new(&[-x, FieldElement::one()]);
-                y_term = y_term.mul_with_ref(&(numerator * denominator_poly));
-            }
-            result = result + y_term;
-        }
-        Ok(result)
+    ) -> Result<Self, String> {
+        Err("Interpolate method shall not be used here".into())
     }
 
     pub fn evaluate<E>(&self, x: &FieldElement<E>) -> FieldElement<E>
